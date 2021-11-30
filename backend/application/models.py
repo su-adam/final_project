aa0cf84b9882afafed1981cc93f874a7a8a9b851
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-11-30 11:23:52 +0000

    creates tables in models.py

--- backend/application/models.py
+++ backend/application/models.py
@@ -1,6 +1,14 @@
 from application import db
 
-class Tasks(db.Model):
+class Galleries(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(30), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+    gallery_name = db.Column(db.String(30), nullable=False)
+    informaiton = db.Column(db.String(30), nullable=False)
+    fee = db.Column(db.Boolean, nullable=False, default=False)
+    location = db.relationship('Location', backref='galleriesbr')
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(30), nullable=False)
+    gallery_id = db.Column(db.String(30), foreign_key=('galleries.id'), nullable=false)
+    
