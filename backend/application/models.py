60f23557aeccd147baf9408ee77fcd459896d596
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-11-30 12:34:59 +0000

    changes to routes

--- backend/application/models.py
+++ backend/application/models.py
@@ -4,11 +4,11 @@ class Galleries(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gallery_name = db.Column(db.String(30), nullable=False)
     informaiton = db.Column(db.String(30), nullable=False)
-    fee = db.Column(db.Boolean, nullable=False, default=False)
+    fee = db.Column(db.String(30), nullable=False)
     location = db.relationship('Location', backref='galleriesbr')
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String(30), nullable=False)
-    gallery_id = db.Column(db.String(30), foreign_key=('galleries.id'), nullable=false)
+    description = db.Column(db.String(30), nullable=False)
+    gallery_id = db.Column(db.Integer, foreign_key=('galleries.id'), nullable=false)
     
--- backend/application/routes.py
+++ backend/application/routes.py
@@ -1,71 +1,57 @@
 from application import app, db
-from application.models import Tasks
+from application.models import Galleries, Location
 from flask import render_template, request, redirect, url_for, jsonify, Response
 
 
  
-@app.route('/create/task', methods=['POST'])
-def create_task():
+@app.route('/create/location', methods=['POST'])
+def create_locaton():
         package = request.json
-        new_task = Tasks(description=package["description"])
-        db.session.add(new_task)
+        new_location = Location(description=package["description"])
+        db.session.add(new_location)
         db.session.commit()
-        return Response(f"Added task with description: {new_task.description}", mimetype='text/plain')
+        return Response(f"New area: {new_location.area}", mimetype='text/plain')
 
 
 
-@app.route('/read/allTasks', methods=['GET'])
-def read_tasks():
-    all_tasks = Tasks.query.all()
-    tasks_dict = {"tasks": []}
-    for task in all_tasks:
-        tasks_dict["tasks"].append(
-            {
-                "id" : task.id,
-                "description": task.description,
-                "completed": task.completed
-            }
-        )
-    return jsonify(tasks_dict)
+# @app.route('/read/locations', methods=['GET'])
+# def read_tasks():
+#     all_tasks = Tasks.query.all()
+#     tasks_dict = {"tasks": []}
+#     for task in all_tasks:
+#         tasks_dict["tasks"].append(
+#             {
+#                 "id" : task.id,
+#                 "description": task.description,
+#                 "completed": task.completed
+#             }
+#         )
+#     return jsonify(tasks_dict)
 
-@app.route('/read/task/<int:id>', methods=['GET'])
-def read_task(id):
-    tasks = Tasks.query.get(id)
-    tasks_dict = {
-                "id" : task.id,
-                "description": task.description,
-                "completed": task.completed
-            }
-    return jsonify(tasks_dict)
+# @app.route('/read/location/<int:id>', methods=['GET'])
+# def read_task(id):
+#     tasks = Tasks.query.get(id)
+#     tasks_dict = {
+#                 "id" : task.id,
+#                 "description": task.description,
+#                 "completed": task.completed
+#             }
+#     return jsonify(tasks_dict)
 
-@app.route('/update/task/<int:id>', methods=['PUT'])
-def update_task(id):
-    package = request.json
-    task = Tasks.query.get(id)
-    task.description = package["description"]
-    db.session.commit()
-    return Response(f" Updated task (ID : {id}) with {task.description}", mimetype='text/plain')
+# @app.route('/update/location/<int:id>', methods=['PUT'])
+# def update_task(id):
+#     package = request.json
+#     task = Tasks.query.get(id)
+#     task.description = package["description"]
+#     db.session.commit()
+#     return Response(f" Updated task (ID : {id}) with {task.description}", mimetype='text/plain')
 
     
 
-@app.route('/delete/task/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    task = Tasks.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return Response(f" Deleted task with ID : {id}", mimetype='text/plain')
+# @app.route('/delete/location/<int:id>', methods=['DELETE'])
+# def delete_task(id):
+#     task = Tasks.query.get(id)
+#     db.session.delete(task)
+#     db.session.commit()
+#     return Response(f" Deleted task with ID : {id}", mimetype='text/plain')
 
-@app.route('/complete/task/<int:id>', methods=['PUT'])
-def complete_task(id):
-    task = Tasks.query.get(id)
-    task.completed = True
-    db.session.commit()
-    return Response(f" Task with ID : {id} set to completed = True", mimetype='text/plain')
-
-
-@app.route('/incomplete/task/<int:id>', methods=['PUT'])
-def incomplete_task(id):
-    task = Tasks.query.get(id)
-    task.completed = False
-    db.session.commit()
-    return Response(f" Task with ID : {id} set to completed = False", mimetype='text/plain')
