2c218faef81bc3ebef2bb60bae79c001a3c76508
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-11-30 12:50:54 +0000

    changes to templates

--- frontend/application/forms.py
+++ frontend/application/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
-class TaskForm(FlaskForm):
-    description = StringField("Task Description", validators=[DataRequired()])
-    submit = SubmitField("Add Task")
+class LocationForm(FlaskForm):
+    location = SelectField("Location in London", choices[])
+    submit = SubmitField("Submit location")
+
