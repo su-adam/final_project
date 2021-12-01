2f3aff22006e5437e18395711566f0caed3d5990
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-12-01 13:55:04 +0000

    change to routes7

--- backend/application/models.py
+++ backend/application/models.py
@@ -9,7 +9,7 @@ class Locations(db.Model):
 class Galleries(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gallery_name = db.Column(db.String(30), nullable=False)
-    informaiton = db.Column(db.String(30), nullable=False)
+    information = db.Column(db.String(30), nullable=False)
     fee = db.Column(db.String(30), nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
 
