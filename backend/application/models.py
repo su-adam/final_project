e9179bf6a47df389b54c4f5d24478bb56533597a
Ubuntu <susanadam@dev-jenkins.ybnbbtvjcqbudaom4odmkkmrlh.parx.internal.cloudapp.net>
2021-12-02 13:34:59 +0000

    changes to for loop in frontend30

--- backend/application/models.py
+++ backend/application/models.py
@@ -2,7 +2,7 @@ from application import db
 
 class Locations(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    country= db.Column(db.String(30), nullable=False)
+    country_name = db.Column(db.String(30), nullable=False)
     city = db.Column(db.String(30), nullable=False)
     galleries = db.relationship('Galleries', backref='locations')
 
@@ -11,7 +11,7 @@ class Galleries(db.Model):
     gallery_name = db.Column(db.String(30), nullable=False)
     information = db.Column(db.String(30), nullable=False)
     fee = db.Column(db.String(30), nullable=False)
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
 
 
     
--- frontend/application/forms.py
+++ frontend/application/forms.py
@@ -3,16 +3,15 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class CreatelocationForm(FlaskForm):
-    location = StringField("Location in London", validators=[DataRequired()])
-    country = StringField("Which country is this gallery located in?", validators=[DataRequired()])
+    country_name = StringField("Which country is this gallery located in?", validators=[DataRequired()])
     city = StringField("Which city is this gallery located in?", validators=[DataRequired()])
     submit = SubmitField("Submit location!")
 
 
 
 class CreategalleryForm(FlaskForm):
-    name = StringField("Name of gallery:", validators=[DataRequired()])
+    gallery_name = StringField("Name of gallery:", validators=[DataRequired()])
     information = StringField("Genre of gallery:", validators=[DataRequired()])
     fee = StringField("Is there a fee for this gallery?", validators=[DataRequired()])
-    location = SelectField("Select country of where yout gallery is located:", choices=[], validators=[DataRequired()])
+    locations = SelectField("Select country of where yout gallery is located:", choices=[], validators=[DataRequired()])
     submit = SubmitField("Submit gallery!")
